8fd74e5e805887db8c2d414c5ecac89681a159af
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-16 11:56:38 -0500

    cambios4

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -9,6 +9,7 @@ import paquete02.DatoPersonal;
 import paquete03.DatosUbicacion;
 import paquete04.DatoAcademico;
 import paquete05.DatoFinal;
+import paquete02.DatoTrabajo;
 
 /**
  *
@@ -20,26 +21,33 @@ public class Principal {
         String nombre;
         String ciudad;
         String apellidoRetornado;
-        
+        String nombreEmpresa;
+        String direccionEmpresa;
+
         double[] misNotas;
         double promedio;
-        
+
         nombre = DatoPersonal.obtenerNombre();
         apellidoRetornado = DatoPersonal.obtenerApellido();
         ciudad = DatosUbicacion.obtenerCiudad();
         misNotas = DatoAcademico.obtenerNotas(4);
         promedio = DatoFinal.obtenerPromedio(misNotas);
-        
-        
+        nombreEmpresa = DatoTrabajo.nombreEmpresa();
+        direccionEmpresa = DatoTrabajo.direccionEmpresa();
+
         System.out.printf("Los datos ingresados son:\n"
                 + "Nombre: %s\n"
                 + "Apellido: %s\n"
                 + "Ciudad: %s\n"
+                + "Nombre Empresa: %s\n"
+                + "Direccion Empresa: %s\n"
                 + "Promedio: %.2f\n",
                 nombre,
                 apellidoRetornado,
                 ciudad,
+                nombreEmpresa,
+                direccionEmpresa,
                 promedio);
     }
-    
+
 }
--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -5,12 +5,29 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class DatoTrabajo {
     // método que permita preguntar al usuario el nombre de la empresa
-    
     // método que permita preguntar al usuario la dirección de la empresa
+    
+    public static String nombreEmpresa() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese nombre de la empresa");
+        String nombreEmpresa = entrada.nextLine();
+
+        return nombreEmpresa;
+    }
+
+    public static String direccionEmpresa() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese direccion de la empresa");
+        String direccionE = entrada.nextLine();
+
+        return direccionE;
+    }
 }
